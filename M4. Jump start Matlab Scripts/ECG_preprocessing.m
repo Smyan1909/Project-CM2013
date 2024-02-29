dd4be069cac44070ca02a6aa8885238c6bf63306
smyan <smyan@sondur.com>
2024-02-29 15:34:29 +0100

    Added wavelet filter for denoising one epoch of an ECG signal

--- M4. Jump start Matlab Scripts/ECG_preprocessing.m
+++ M4. Jump start Matlab Scripts/ECG_preprocessing.m
@@ -37,3 +37,51 @@ epoch_ecg_end = epoch_ecg_start + 30*Fs;
 ecg_signal= -record(4, epoch_ecg_start:epoch_ecg_end);
 plot((1:length(ecg_signal))/Fs, ecg_signal);
 xlim([1 30]);
+
+%% Wavelet Filtering
+levels = 6; 
+wname = "sym8";
+
+[c, l] = wavedec(ecg_signal, levels, wname);
+
+approx = appcoef(c,l,wname);
+[cd1,cd2,cd3, cd4, cd5, cd6] = detcoef(c,l,[1 2 3 4 5 6]);
+
+figure
+tiledlayout(7,1)
+nexttile
+plot(approx)
+title("Approximation Coefficients")
+nexttile
+plot(cd6)
+title("Level 6 Detail Coefficients")
+nexttile
+plot(cd5)
+title("Level 5 Detail Coefficients")
+nexttile
+plot(cd4)
+title("Level 4 Detail Coefficients")
+nexttile
+plot(cd3)
+title("Level 3 Detail Coefficients")
+nexttile
+plot(cd2)
+title("Level 2 Detail Coefficients")
+nexttile
+plot(cd1)
+title("Level 1 Detail Coefficients")
+
+
+max_level_for_denoise = wmaxlev(length(ecg_signal), wname);
+ecg_denoised = wdenoise(ecg_signal, max_level_for_denoise, Wavelet=wname, DenoisingMethod="Minimax");
+
+figure
+tiledlayout(2,1)
+nexttile
+plot((1:length(ecg_signal))/Fs, ecg_signal)
+title("Unfiltered ECG signal")
+xlim([1 30]);
+nexttile
+plot((1:length(ecg_signal))/Fs, ecg_denoised)
+title("Filtered ECG signal")
+xlim([1 30]);
