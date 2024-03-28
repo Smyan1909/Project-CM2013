5f4c6342869809427a5ee5bb3845ece2c0691e12
smyan <smyan@sondur.com>
2024-03-28 14:01:55 +0100

    Added spectral feature extraction and changed datatypes to structs

---
+++ feature_extraction/spectral_feature_extraction.m
@@ -0,0 +1,26 @@
+function spec_features = spectral_feature_extraction(signal, Fs)
+    %SPECTRAL FEATURE EXTRACTION Summary of this function goes here
+    [spectrum,f] = pwelch(signal,[],[],[],Fs);
+    
+    centroid = sum(spectrum.*f)/sum(spectrum);
+
+    SFM = geomean(abs(spectrum))/mean(spectrum);
+    
+    spect_sum = sum(spectrum);
+
+    i=1; part_sum=0;
+
+    while(part_sum < spect_sum*0.85)
+        
+        part_sum = part_sum + spectrum(i);
+        
+        i = i + 1;
+        
+    end
+    
+    SR = f(i-1);
+
+    spec_features = struct('Centroid', centroid, 'SFM', SFM, 'SR', SR);
+
+end
+
--- feature_extraction/temporal_feature_extraction.m
+++ feature_extraction/temporal_feature_extraction.m
@@ -1,5 +1,6 @@
 function temp_features = temporal_feature_extraction(signal, Fs)
-    %TEMPORAL_FEATURE_EXTRACTION Summary of this function goes here
+    %TEMPORAL_FEATURE_EXTRACTION extracts temporal features from a given
+    %signal
     
     
     mean_voltage = mean(signal);
@@ -32,7 +33,14 @@ function temp_features = temporal_feature_extraction(signal, Fs)
     
     mpv = max(abs(signal));
 
-    temp_features = [mean_voltage, activity, skew, kurt, mobility_y, complexity, mav, rms, wl, sum_abs, signal_energy, 
-                    mpv];
+    %temp_features = [mean_voltage, activity, skew, kurt, mobility_y, complexity, mav, rms, wl, sum_abs, signal_energy, 
+        %mpv];
+
+    temp_features = struct('Mean_Voltage', mean_voltage, 'Activity', activity, ...
+                            'Skewness', skew, 'Kurtosis', kurt, 'Mobility', ...
+                            mobility_y, 'Complexity', complexity, ...
+                            'Mean_Absolute_Value', mav, 'RMS', rms, ...
+                            "WL", wl, "Sum_Abs", sum_abs, "Signal_Energy", ...
+                            signal_energy, "Max_Peak_Val", mpv);
 end
 
