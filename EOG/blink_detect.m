c2c8a935ab575898c765bca32ac66727c187bbbe
yaaun <6322902+yaaun@users.noreply.github.com>
2024-03-31 00:10:54 +0100

    Blink detection algorithm stub

---
+++ EOG/blink_detect.m
@@ -0,0 +1,20 @@
+function blink = blink_detect(signals, fs)
+arguments
+    signals double
+    fs double
+    smooth_secs = 0.5
+end
+
+filted = medfilt1(signals, 3, [], 2);
+
+% Bandpass filtering here...
+
+squared = filted^2;
+
+smoothed = movmean(squared, smooth_secs * fs, 2);
+
+diffed = diff(signals, 1, 2);
+
+smoothdiffed = movmean(diffed, smooth_secs * fs, 2);
+
+end
