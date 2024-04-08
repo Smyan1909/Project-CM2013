81bda80515034674ea89bac1538de27071fa7b0e
Tomasz Kuliński <6322902+yaaun@users.noreply.github.com>
2024-04-08 17:12:54 +0200

    Experimentation with the EOG blink detection algorithm.

--- livescript/generate_filtered_EOG.m
+++ livescript/generate_filtered_EOG.m
@@ -12,7 +12,10 @@ chebFilted = filter(chebFiltHd, signals');
 
 parzenFiltHd = parzenFIR_EOG_blink();
 parzenFilted = filter(parzenFiltHd, signals');
-
+% 
+% structToWrite = struct()
+% edfwrite('_preproc_out', struct())
+%% plotting
 hold on;
 
 plot(ts, signals(1, idxs), 'c', DisplayName='Original');
@@ -23,11 +26,23 @@ legend();
 zoom xon;
 
 zz = zoom();
-zz.Enabled = 'on';
+zz.Enable = 'on';
 zz.Motion = 'horizontal';
 
-
+%%
 parzenFiltedSmooth = movmean(parzenFilted.^2, 18, 1);
+parzenFiltedSmoother = movmean(parzenFilted.^2, 36, 1);
+parzenFiltedRougher = movmean(parzenFilted.^2, 9, 1);
 
-plot(ts, parzenFiltedSmooth(idxs, 1), 'g', DisplayName='Detection function');
+normAmp = 1 / rms(parzenFilted(idxs, 1));
+
+hold on;
+zoom xon;
+
+zz = zoom();
+zz.Enable = 'on';
+zz.Motion = 'horizontal';
+plot(ts, parzenFiltedSmooth(idxs, 1) * normAmp, 'g', DisplayName='Detection function 18 mean = 360 ms');
+plot(ts, parzenFiltedSmoother(idxs, 1) * normAmp, 'y', DisplayName='Detection function 36 mean = 720 ms');
+plot(ts, parzenFiltedRougher(idxs, 1) * normAmp, 'b', DisplayName='Detection function 9 mean = 180 ms');
 legend();
