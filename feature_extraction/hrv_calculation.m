dd9bc9a5ff25118619519c9845d1138a1a530ce8
smyan <smyan@sondur.com>
2024-04-10 14:17:14 +0200

    Fixed more bugs that occured due to indexing errors in k_complex and hrv detection

--- feature_extraction/hrv_calculation.m
+++ feature_extraction/hrv_calculation.m
@@ -2,7 +2,7 @@ function rmssd = hrv_calculation(ECG_signal, Fs)
     %HRV_CALCULATION Algorithm for hrv using RMSSD method
     
     %Find the positive R-peaks
-    [peaks, peak_locs] = findpeaks(ECG_signal, 'MinPeakHeight', 0.6*max(ecg_denoised));
+    [peaks, peak_locs] = findpeaks(ECG_signal, 'MinPeakHeight', 0.6*max(ECG_signal));
     
     % Compute time differences between successive R-peaks
     rr_intervals = diff(peak_locs) / Fs;  % Convert peak locations to time (in seconds)
--- feature_extraction/k_complex_detection.m
+++ feature_extraction/k_complex_detection.m
@@ -11,9 +11,6 @@ function k_complex_exist = k_complex_detection(signal, Fs)
     b = (1/windowSize)*ones(1,windowSize);
     a = 1;
     eeg_filtered = filter(b, a, eeg_filtered);
-    figure
-    plot((1:length(eeg_signal))/Fs, eeg_filtered);
-    xlim([1 30]);
     
     %Find the negative and positive peaks in the signal
     
@@ -26,11 +23,13 @@ function k_complex_exist = k_complex_detection(signal, Fs)
     
     %Sort the locations of the peaks
     all_peak_locs = sort([negative_peak_locs, positive_peak_locs]);
+
+    exceed_threshold_locs = [];
     
     %If the first peak is negative and the second peak is positive and if
     %they are above the threshold of 65 microV then save the indexes (locations)
     for i=1:length(all_peak_locs)-1
-        if eeg_filtered(all_peak_locs(i)) > 0 && eeg_filtered(all_peak_locs(i+1)) < 0
+        if (eeg_filtered(all_peak_locs(i)) > 0) && (eeg_filtered(all_peak_locs(i+1)) < 0)
             if abs(eeg_filtered(all_peak_locs(i+1)) - eeg_filtered(all_peak_locs(i))) >= 65
                 exceed_threshold_locs = [exceed_threshold_locs, i];
             end
@@ -42,11 +41,11 @@ function k_complex_exist = k_complex_detection(signal, Fs)
     %Check if the number of values that exceed the threshold is within the
     %K-complex time width and if it is register it as a K-complex
     for i=1:length(exceed_threshold_locs)
-        
-        if abs(all_peak_locs(exceed_threshold_locs(i)-1)/Fs - all_peak_locs(exceed_threshold_locs(i)+2)/Fs) >= 1.2 && abs(all_peak_locs(exceed_threshold_locs(i)-1)/Fs - all_peak_locs(exceed_threshold_locs(i)+2)/Fs) <= 1.5
-            k_complex_indice_start = [k_complex_indice_start, all_peak_locs(exceed_threshold_locs(i)-1)];
+        if (exceed_threshold_locs(i) > 1 && (exceed_threshold_locs(i) + 2) <= length(all_peak_locs))
+            if abs(all_peak_locs(exceed_threshold_locs(i)-1)/Fs - all_peak_locs(exceed_threshold_locs(i)+2)/Fs) >= 1.2 && abs(all_peak_locs(exceed_threshold_locs(i)-1)/Fs - all_peak_locs(exceed_threshold_locs(i)+2)/Fs) <= 1.5
+                k_complex_indice_start = [k_complex_indice_start, all_peak_locs(exceed_threshold_locs(i)-1)];
+            end
         end
-    
     end
     
     %Binary check to see if K-Complex exists or not
