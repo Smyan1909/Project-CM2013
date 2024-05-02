6c5a36ff823cfe2691e5abf91cd2746bd5239d8b
Tomasz Kuliński <6322902+yaaun@users.noreply.github.com>
2024-05-02 15:01:55 +0200

    Changes from MATLAB Online (which were too difficult to commit directly from there)

--- livescript/test_main_visual_EOG_feature_extraction.m
+++ livescript/test_main_visual_EOG_feature_extraction.m
@@ -1,18 +1,24 @@
-[~ , eogr] = load_EOG(4, 'R');
+[header, eogr, epochInfo] = load_EOG(4, 'R');
 
 lyout = tiledlayout(5, 1, TileSpacing="none");
 
 preprocd = preprocess_EOG_blink(eogr, 50);
 
-idxs = 1:100000;
-tsEOG = (0:length(preprocd(1, idxs)) - 1) / 50;
+startT = 500;
+endT = 1000;
+Fs = 50;
+idxs = 1 + startT * Fs : endT*Fs;
+tsEOG = (0:length(preprocd(1, idxs)) - 1) / Fs;
 
 ax1 = nexttile([4, 1]);
 plot(tsEOG, eogr(1, idxs), tsEOG, preprocd(1, idxs));
 
 ax2 = nexttile([1, 1]);
 
-plot(epochIdxs*30, epochInfo.stages(epochIdxs));
+EPOCH_DUR = 30;
+epoch_idxs = 1:endT/EPOCH_DUR;
+epoch_ts = tsEOG(1:Fs*EPOCH_DUR:end);
+bar(epoch_ts, epochInfo.stages(epoch_idxs));
 yticks(0:6);
 yticklabels({'REM','','N3','N2','N1','Wake',''});
 
